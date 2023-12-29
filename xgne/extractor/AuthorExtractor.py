d06d6586be277bf0d9709d3b0fbd62aa633c6b34
brucelong <18656170559@>
2023-12-29 16:29:00 +0800

    调整为中文注释

--- xgne/extractor/AuthorExtractor.py
+++ xgne/extractor/AuthorExtractor.py
@@ -17,16 +17,19 @@ class AuthorExtractor:
         self.authors: List[str] = []
 
     def extractor(self, element: HtmlElement, author_xpath='') -> List[str]:
+        # 如果提供了自定义的作者 XPath，则直接使用该 XPath 进行提取
         author_xpath = author_xpath or config.get('author', {}).get('xpath')
         if author_xpath:
             self.authors = element.xpath(author_xpath)
+        # 否则，从文本中提取作者信息
         text = ''.join(element.xpath('.//text()'))
         for pattern in self.author_pattern:
             author_obj = re.search(pattern, text)
             if author_obj:
                 self.authors = author_obj.group
                 break
-        self.authors = [i for i in self.authors if i.strip()]
+        self.authors = [i.strip() for i in self.authors if i.strip()]
+        # 如果未成功提取作者信息，则尝试从 JSON-LD 数据和其他元素中提取
         if not self.authors:
             _digits = re.compile(r"\d")
             author_stopwords_patt = [re.escape(x) for x in AUTHOR_STOP_WORDS]
@@ -38,14 +41,15 @@ class AuthorExtractor:
                 return bool(_digits.search(d))
 
             def uniqify_list(lst: List[str]) -> List[str]:
-                """Remove duplicates from provided list but maintain original order.
-                Ignores trailing spaces and case.
+                """
+                从提供的字符串列表中去除重复项但保持原始顺序。
+                忽略末尾空格和大小写。
 
                 Args:
-                    lst (List[str]): Input list of strings, with potential duplicates
+                    lst (List[str]): 输入的字符串列表，可能包含重复项
 
                 Returns:
-                    List[str]: Output list of strings, with duplicates removed
+                    List[str]: 输出的字符串列表，已删除重复项
                 """
                 seen = OrderedDict()
                 for item in lst:
@@ -54,31 +58,22 @@ class AuthorExtractor:
 
             def parse_byline(search_str):
                 """
-                Takes a candidate line of html or text and
-                extracts out the name(s) in list form:
-                parse_byline('<div>By: <strong>Lucas Ou-Yang</strong>,
-                        <strong>Alex Smith</strong></div>')
-                ['Lucas Ou-Yang', 'Alex Smith']
+                解析候选行的 HTML 或文本，并以列表形式提取出名字
                 """
-                # Remove HTML boilerplate
+                # 移除 HTML 标签
                 search_str = re.sub("<[^<]+?>", "", search_str)
                 search_str = re.sub("[\n\t\r\xa0]", " ", search_str)
 
-                # Remove original By statement
+                # 移除原始的 "By" 语句
                 m = re.search(r"\b(by|from)[:\s](.*)", search_str, flags=re.IGNORECASE)
                 if m:
                     search_str = m.group(2)
 
                 search_str = search_str.strip()
 
-                # Chunk the line by non alphanumeric
-                # tokens (few name exceptions)
-                # >>> re.split("[^\w\'\-\.]",
-                #           "Tyler G. Jones, Lucas Ou, Dean O'Brian and Ronald")
-                # ['Tyler', 'G.', 'Jones', '', 'Lucas', 'Ou', '',
-                #           'Dean', "O'Brian", 'and', 'Ronald']
+                # 使用非字母数字字符分割行
                 name_tokens = re.split(r"[·,\|]|\sand\s|\set\s|\sund\s|/", search_str)
-                # some sanity checks
+                # 一些合理性检查
                 name_tokens = [s.strip() for s in name_tokens if not contains_digits(s)]
                 name_tokens = [s for s in name_tokens if 5 > len(re.findall(r"\w+", s)) > 1]
 
@@ -121,11 +116,8 @@ class AuthorExtractor:
                         get_authors(script_tag["author"])
 
             def get_text_from_element(node: HtmlElement) -> str:
-                """Return the text from an element, including the text from its children
-                Args:
-                    node (lxml.html.HtmlElement): Input node
-                Returns:
-                    str: Text from the node
+                """
+                从元素中提取文本，包括其子元素的文本
                 """
                 if node is None:
                     return ""
@@ -150,24 +142,23 @@ class AuthorExtractor:
             # TODO: be more specific, not a combination of all attributes and values
             for attr in AUTHOR_ATTRS:
                 for val in AUTHOR_VALS:
-                    # found = doc.xpath('//*[@%s="%s"]' % (attr, val))
                     found = self.parser.get_element_by_attribs(element, attribs={attr: val})
                     matches.extend([(found, getpath(found)) for found in found])
 
             matches.sort(
                 key=lambda x: x[1], reverse=True
-            )  # sort by xpath. we want the most specific match
+            )  # 按 XPath 排序，以获取最特定的匹配项
             matches_reduced = []
             for m in matches:
                 if len(matches_reduced) == 0:
                     matches_reduced.append(m)
                 elif not matches_reduced[-1][1].startswith(
                         m[1]
-                ):  # remove parents of previous node
+                ):  # 移除前一个节点的父节点
                     matches_reduced.append(m)
             matches_reduced.sort(
                 key=lambda x: x[1]
-            )  # Preserve some sort of order for the authors
+            )  # 保留一些作者的排序
 
             for match, _ in matches_reduced:
                 content: Union[str, List] = ""
@@ -176,13 +167,13 @@ class AuthorExtractor:
                     if len(mm) > 0:
                         content = mm[0]
                 else:
-                    # TODO: ignore <time> tags, or tags with "on ..."
-                    # TODO: remove <style> tags https://washingtonindependent.com/how-to-apply-for-reseller-permit-in-washington-state/
+                    # 忽略 <time> 标签或带有 "on ..." 的标签
+                    # 移除 <style> 标签 https://washingtonindependent.com/how-to-apply-for-reseller-permit-in-washington-state/
                     content = get_text_from_element(match)
                 if len(content) > 0:
                     authors.extend(parse_byline(content))
 
-            # Clean up authors of stopwords such as Reporter, Senior Reporter
+            # 输出的字符串列表，已删除重复项
             authors = [re.sub(author_stopwords, "", x).strip(" .,-/") for x in authors]
             self.authors = uniqify_list(authors)
         return self.authors
